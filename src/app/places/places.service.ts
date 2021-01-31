2ce76b7934511590b4bd6011c793ee35ae4c9d0f
utkudogan <52001660+utkudogan@users.noreply.github.com>
2021-01-31 23:39:37 +0300

    uygulamanın sayfaları eklendi.
    yerlerin listelenmesi yapıldı

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { Place } from "./place.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      "p1",
+      "Kayseri",
+      "anadolunun ortası",
+      "http://www.agu.edu.tr/userfiles/up/life-in-kayseri.jpeg",
+      38.38
+    ),
+    new Place(
+      "p2",
+      "Ankara",
+      "anadolunun başkenti",
+      "https://cdnp.flypgs.com/files/Sehirler-long-tail/Ankara/Ankara_Sehir_Rehberi.jpg",
+      6.06
+    ),
+    new Place(
+      "p3",
+      "İstanbul",
+      "boğazın çevresi",
+      "https://www.hisglobal.com.tr/assets/images/1581429467.jpg",
+      34.34
+    ),
+  ];
+
+  getPlaces() {
+    return [...this._places];
+  }
+  constructor() {}
+}
